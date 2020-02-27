38f79c25f1efdfdfeaf88cfd187ca4061735fdaf
Matt Biddulph <mb@hackdiary.com>
2020-02-28 01:53:16 +0700

    BPM fix

--- softbeats/softbeats.lua
+++ softbeats/softbeats.lua
@@ -5,13 +5,13 @@ local beat_clock
 
 function init_beatclock(bpm)
   beat_clock = BeatClock.new()
-  beat_clock:bpm_change(bpm)
   beat_clock.ticks_per_step = 6
   beat_clock.steps_per_beat = 2
   beat_clock.on_select_internal = function() beat_clock:start() end
   beat_clock.on_select_external = function() print("external") end
   beat_clock:add_clock_params()
   beat_clock:start()
+  beat_clock:bpm_change(bpm)
 
   local clk_midi = midi.connect(1)
   clk_midi.event = beat_clock.process_mid
